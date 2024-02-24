50d92c19680404dd723a8949f29bf17261348d26
Jakub Czyz <jakub12134@gmail.com>
2024-02-24 16:03:27 +0100

    update addr

--- main.go
+++ main.go
@@ -6,13 +6,17 @@ import (
 	"net/http"
 )
 
+var (
+	addr = ":80"
+)
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello world")
 	})
 
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(addr, mux); err != nil {
 		log.Fatal(err)
 	}
 }
